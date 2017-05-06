b3e59893db93db2be62bf09194dab49c0c8c8699
jkhoang313 <jkhoang313@gmail.com>
2017-05-05 23:51:23 -0400

    Added all cards to reduceR

--- src/helpers/cards.js
+++ src/helpers/cards.js
@@ -6,7 +6,62 @@ import Barbarians from "../../public/images/Barbarians.png"
 import Bomber from "../../public/images/Bomber.png"
 import BombTower from "../../public/images/BombTower.png"
 import Bowler from "../../public/images/Bowler.png"
+import Cannon from "../../public/images/Cannon.png"
+import Clone from "../../public/images/Clone.png"
+import DarkPrince from "../../public/images/DarkPrince.png"
+import EliteBarbarians from "../../public/images/EliteBarbarians.png"
+import ElixirCollector from "../../public/images/ElixirCollector.png"
+import Fireball from "../../public/images/Fireball.png"
+import FireSpirits from "../../public/images/FireSpirits.png"
+import Freeze from "../../public/images/Freeze.png"
+import Furnace from "../../public/images/Furnace.png"
+import Giant from "../../public/images/Giant.png"
+import GiantSkeleton from "../../public/images/GiantSkeleton.png"
+import GoblinBarrel from "../../public/images/GoblinBarrel.png"
+import GoblinHut from "../../public/images/GoblinHut.png"
 import Goblins from "../../public/images/Goblins.png"
+import Golem from "../../public/images/Golem.png"
+import Graveyard from "../../public/images/Graveyard.png"
+import Guards from "../../public/images/Guards.png"
+import HogRider from "../../public/images/HogRider.png"
+import IceGolem from "../../public/images/IceGolem.png"
+import IceSpirit from "../../public/images/IceSpirit.png"
+import IceWizard from "../../public/images/IceWizard.png"
+import InfernoDragon from "../../public/images/InfernoDragon.png"
+import InfernoTower from "../../public/images/InfernoTower.png"
+import Knight from "../../public/images/Knight.png"
+import LavaHound from "../../public/images/LavaHound.png"
+import Lightning from "../../public/images/Lightning.png"
+import Lumberjack from "../../public/images/Lumberjack.png"
+import MegaMinion from "../../public/images/MegaMinion.png"
+import Miner from "../../public/images/Miner.png"
+import MinionHorde from "../../public/images/MinionHorde.png"
+import Minions from "../../public/images/Minions.png"
+import MiniPEKKA from "../../public/images/MiniPEKKA.png"
+import Mirror from "../../public/images/Mirror.png"
+import Mortar from "../../public/images/Mortar.png"
+import Musketeer from "../../public/images/Musketeer.png"
+import PEKKA from "../../public/images/PEKKA.png"
+import Poison from "../../public/images/Poison.png"
+import Prince from "../../public/images/Prince.png"
+import Princess from "../../public/images/Princess.png"
+import Rage from "../../public/images/Rage.png"
+import Rocket from "../../public/images/Rocket.png"
+import RoyalGiant from "../../public/images/RoyalGiant.png"
+import SkeletonArmy from "../../public/images/SkeletonArmy.png"
+import Skeletons from "../../public/images/Skeletons.png"
+import Sparky from "../../public/images/Sparky.png"
+import SpearGoblins from "../../public/images/SpearGoblins.png"
+import Tesla from "../../public/images/Tesla.png"
+import TheLog from "../../public/images/TheLog.png"
+import ThreeMusketeers from "../../public/images/ThreeMusketeers.png"
+import Tombstone from "../../public/images/Tombstone.png"
+import Tornado from "../../public/images/Tornado.png"
+import Valkyrie from "../../public/images/Valkyrie.png"
+import Witch from "../../public/images/Witch.png"
+import Wizard from "../../public/images/Wizard.png"
+import XBow from "../../public/images/X-Bow.png"
+import Zap from "../../public/images/Zap.png"
 
 export const cards = {
   "Archers": {
@@ -49,9 +104,284 @@ export const cards = {
     image: Bowler,
     elixirCost: 5
   },
+  "Cannon": {
+    name: "Cannon",
+    image: Cannon,
+    elixirCost: 3
+  },
+  "Clone": {
+    name: "Clone",
+    image: Clone,
+    elixirCost: 3
+  },
+  "DarkPrince": {
+    name: "DarkPrince",
+    image: DarkPrince,
+    elixirCost: 4
+  },
+  "EliteBarbarians": {
+    name: "EliteBarbarians",
+    image: EliteBarbarians,
+    elixirCost: 6
+  },
+  "ElixirCollector": {
+    name: "ElixirCollector",
+    image: ElixirCollector,
+    elixirCost: 6
+  },
+  "Fireball": {
+    name: "Fireball",
+    image: Fireball,
+    elixirCost: 4
+  },
+  "FireSpirits": {
+    name: "FireSpirits",
+    image: FireSpirits,
+    elixirCost: 2
+  },
+  "Freeze": {
+    name: "Freeze",
+    image: Freeze,
+    elixirCost: 4
+  },
+  "Furnace": {
+    name: "Furnace",
+    image: Furnace,
+    elixirCost: 4
+  },
+  "Giant": {
+    name: "Giant",
+    image: Giant,
+    elixirCost: 5
+  },
+  "GiantSkeleton": {
+    name: "GiantSkeleton",
+    image: GiantSkeleton,
+    elixirCost: 6
+  },
+  "GoblinBarrel": {
+    name: "GoblinBarrel",
+    image: GoblinBarrel,
+    elixirCost: 3
+  },
+  "GoblinHut": {
+    name: "GoblinHut",
+    image: GoblinHut,
+    elixirCost: 3
+  },
   "Goblins": {
     name: "Goblins",
     image: Goblins,
     elixirCost: 2
   },
+  "Golem": {
+    name: "Golem",
+    image: Golem,
+    elixirCost: 8
+  },
+  "Graveyard": {
+    name: "Graveyard",
+    image: Graveyard,
+    elixirCost: 5
+  },
+  "Guards": {
+    name: "Guards",
+    image: Guards,
+    elixirCost: 3
+  },
+  "HogRider": {
+    name: "HogRider",
+    image: HogRider,
+    elixirCost: 4
+  },
+  "IceGolem": {
+    name: "IceGolem",
+    image: IceGolem,
+    elixirCost: 2
+  },
+  "IceSpirit": {
+    name: "IceSpirit",
+    image: IceSpirit,
+    elixirCost: 1
+  },
+  "IceWizard": {
+    name: "IceWizard",
+    image: IceWizard,
+    elixirCost: 3
+  },
+  "InfernoDragon": {
+    name: "InfernoDragon",
+    image: InfernoDragon,
+    elixirCost: 4
+  },
+  "InfernoTower": {
+    name: "InfernoTower",
+    image: InfernoTower,
+    elixirCost: 5
+  },
+  "Knight": {
+    name: "Knight",
+    image: Knight,
+    elixirCost: 3
+  },
+  "LavaHound": {
+    name: "LavaHound",
+    image: LavaHound,
+    elixirCost: 7
+  },
+  "Lightning": {
+    name: "Lightning",
+    image: Lightning,
+    elixirCost: 6
+  },
+  "Lumberjack": {
+    name: "Lumberjack",
+    image: Lumberjack,
+    elixirCost: 4
+  },
+  "MegaMinion": {
+    name: "MegaMinion",
+    image: MegaMinion,
+    elixirCost: 3
+  },
+  "Miner": {
+    name: "Miner",
+    image: Miner,
+    elixirCost: 3
+  },
+  "MinionHorde": {
+    name: "MinionHorde",
+    image: MinionHorde,
+    elixirCost: 5
+  },
+  "Minions": {
+    name: "Minions",
+    image: Minions,
+    elixirCost: 3
+  },
+  "MiniPEKKA": {
+    name: "MiniPEKKA",
+    image: MiniPEKKA,
+    elixirCost: 4
+  },
+  "Mirror": {
+    name: "Mirror",
+    image: Mirror,
+    elixirCost: "?"
+  },
+  "Mortar": {
+    name: "Mortar",
+    image: Mortar,
+    elixirCost: 4
+  },
+  "Musketeer": {
+    name: "Musketeer",
+    image: Musketeer,
+    elixirCost: 4
+  },
+  "PEKKA": {
+    name: "PEKKA",
+    image: PEKKA,
+    elixirCost: 7
+  },
+  "Poison": {
+    name: "Poison",
+    image: Poison,
+    elixirCost: 4
+  },
+  "Prince": {
+    name: "Prince",
+    image: Prince,
+    elixirCost: 5
+  },
+  "Princess": {
+    name: "Princess",
+    image: Princess,
+    elixirCost: 3
+  },
+  "Rage": {
+    name: "Rage",
+    image: Rage,
+    elixirCost: 2
+  },
+  "Rocket": {
+    name: "Rocket",
+    image: Rocket,
+    elixirCost: 6
+  },
+  "RoyalGiant": {
+    name: "RoyalGiant",
+    image: RoyalGiant,
+    elixirCost: 6
+  },
+  "SkeletonArmy": {
+    name: "SkeletonArmy",
+    image: SkeletonArmy,
+    elixirCost: 3
+  },
+  "Skeletons": {
+    name: "Skeletons",
+    image: Skeletons,
+    elixirCost: 1
+  },
+  "Sparky": {
+    name: "Sparky",
+    image: Sparky,
+    elixirCost: 6
+  },
+  "SpearGoblins": {
+    name: "SpearGoblins",
+    image: SpearGoblins,
+    elixirCost: 2
+  },
+  "Tesla": {
+    name: "Tesla",
+    image: Tesla,
+    elixirCost: 4
+  },
+  "TheLog": {
+    name: "TheLog",
+    image: TheLog,
+    elixirCost: 2
+  },
+  "ThreeMusketeers": {
+    name: "ThreeMusketeers",
+    image: ThreeMusketeers,
+    elixirCost: 9
+  },
+  "Tombstone": {
+    name: "Tombstone",
+    image: Tombstone,
+    elixirCost: 3
+  },
+  "Tornado": {
+    name: "Tornado",
+    image: Tornado,
+    elixirCost: 3
+  },
+  "Valkyrie": {
+    name: "Valkyrie",
+    image: Valkyrie,
+    elixirCost: 4
+  },
+  "Witch": {
+    name: "Witch",
+    image: Witch,
+    elixirCost: 5
+  },
+  "Wizard": {
+    name: "Wizard",
+    image: Wizard,
+    elixirCost: 5
+  },
+  "X-Bow": {
+    name: "X-Bow",
+    image: XBow,
+    elixirCost: 6
+  },
+  "Zap": {
+    name: "Zap",
+    image: Zap,
+    elixirCost: 2
+  }
 }
--- src/reducers/tierListReducer.js
+++ src/reducers/tierListReducer.js
@@ -15,7 +15,7 @@ export default function(state={
       cards: ["Barbarian Hut"]
     }
   ],
-  cardsRemaining: ["Archers", "Arrows", "Balloon", "Barbarians", "Goblins"]
+  cardsRemaining: ["Archers", "Arrows", "Balloon", "Barbarians", "Cannon", "Clone", "DarkPrince", "EliteBarbarians", "ElixirCollector", "Fireball", "FireSpirits", "Freeze", "Furnace", "Giant", "GiantSkeleton", "GoblinBarrel", "GoblinHut", "Goblins", "Golem", "Graveyard", "Guards", "HogRider", "IceGolem", "IceSpirit", "IceWizard", "InfernoDragon", "InfernoTower", "Knight", "LavaHound", "Lightning", "Lumberjack", "MegaMinion", "Miner", "MinionHorde", "Minions", "MiniPEKKA", "Mirror", "Mortar", "Musketeer", "PEKKA", "Poison", "Prince", "Princess", "Rage", "Rocket", "RoyalGiant", "SkeletonArmy", "Skeletons", "Sparky", "SpearGoblins", "Tesla", "TheLog", "ThreeMusketeers", "Tombstone", "Tornado", "Valkyrie", "Witch", "Wizard", "X-Bow", "Zap"]
 }, action) {
   switch (action.type) {
     default:
