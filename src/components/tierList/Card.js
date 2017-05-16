8785e665fb6b428d8b413b23bf7dff9e86ba8334
jkhoang313 <jkhoang313@gmail.com>
2017-05-15 23:09:16 -0400

    WIP: popover for cards

--- src/components/tierList/Card.js
+++ src/components/tierList/Card.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-import ReactTooltip from 'react-tooltip'
+import ReactTooltip from 'react-tooltip';
+import { Popover, PopoverTitle, PopoverContent } from 'reactstrap';
 
 import { cardDescriptions } from '../../helpers/cards';
 
 
 export default class Card extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      cardPopoverOpen: false
+    }
+
+    this.togglePopover = this.togglePopover.bind(this)
+  }
+
+  togglePopover() {
+    this.setState({
+      cardPopoverOpen: !this.state.cardPopoverOpen
+    });
+  }
+
   render() {
     const { name } = this.props;
     const card = cardDescriptions[name];
+    const divId = name.toLowerCase().replace(/\s+/g, '-') + "-card";
 
     return (
-      <div className="card-icon" id={name.toLowerCase()+"-card"}>
+      <div className="card-icon" id={divId}>
         <ReactTooltip
           id="card-description"
           type="info"/>
@@ -19,7 +37,16 @@ export default class Card extends Component {
           className="card-image"
           alt={card.name}
           data-for="card-description"
-          data-tip={name}/>
+          data-tip={name}
+          onClick={this.togglePopover}/>
+        <Popover
+          placement="top"
+          isOpen={this.state.cardPopoverOpen}
+          target={divId}
+          toggle={this.toggle}>
+          <PopoverTitle>Popover Title</PopoverTitle>
+          <PopoverContent>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverContent>
+        </Popover>
       </div>
     );
   };
